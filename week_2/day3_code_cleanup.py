a669e083891d3a611aa034442b94f7ffff117277
tdhuynh <tommyhuynh93@gmail.com>
2016-09-21 09:57:23 -0400

    input and inventory working

--- week_2/day3_code_cleanup.py
+++ week_2/day3_code_cleanup.py
@@ -6,7 +6,13 @@
 
 player = {}
 player_name = input("Welcome Traveler! What is your name? ")
-player['name'] = player_name
+player["name"] = player_name
 print(player)
 
-# purposefully writing code that is redundant
+while True:
+    choice = input("Would you like to (i)nspect your inventory? or (a)dd to inventory?\n>>> ")
+
+    if choice == 'a':
+        print("adding item to inventory")
+    elif choice == 'i':
+        print("looking at inventory")
