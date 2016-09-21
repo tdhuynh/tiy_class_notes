dfebe79e036abc810709cd55e88eab7f87602cb4
tdhuynh <tommyhuynh93@gmail.com>
2016-09-21 09:41:26 -0400

    creating redundant code to clean up

---
+++ week_2/day3_code_cleanup.py
@@ -0,0 +1,12 @@
+# Inventory Tracker RPG
+
+# Player
+# Inventory
+# Items
+
+player = {}
+player_name = input("Welcome Traveler! What is your name? ")
+player['name'] = player_name
+print(player)
+
+# purposefully writing code that is redundant
