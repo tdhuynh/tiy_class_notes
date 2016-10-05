58aca89f767730417d40c4cf607b5155bfe3c948
tdhuynh <tommyhuynh93@gmail.com>
2016-10-05 12:17:51 -0400

    notes on migrations and many other things

--- week4/playground/my_app/models.py
+++ week4/playground/my_app/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
+class Fear(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
 class Person(models.Model):
     name = models.CharField(max_length=30)
     age = models.IntegerField()
     eye_color = models.CharField(max_length=15)
+    best_friend = models.ForeignKey("self", null=True)
+    fears = models.ManyToManyField(Fear)
 
+    def __str__(self):
+        return self.name
 
 class Team(models.Model):
     name = models.CharField(max_length=100)
     year = models.IntegerField()
     captain = models.ForeignKey(Person) # THIS IS THE WAY TO CREATE A FOREIGN KEY
-        
