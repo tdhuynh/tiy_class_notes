df96216f65a41ed30675ecb4daaeea1c2688afb3
tdhuynh <tommyhuynh93@gmail.com>
2016-10-06 09:49:46 -0400

    created model and registered with admin

--- week4/lyrics/record_store/admin.py
+++ week4/lyrics/record_store/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from record_store.models import Song
 # Register your models here.
+
+admin.site.register(Song)
--- week4/lyrics/record_store/models.py
+++ week4/lyrics/record_store/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Song(models.Model):
+    artist = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    lyrics = models.TextField()
+    release_year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
