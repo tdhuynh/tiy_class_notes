1711ee83f38cabbdd462770241d15f330e712d09
tdhuynh <tommyhuynh93@gmail.com>
2016-10-10 12:19:47 -0400

    html and css notes

---
+++ week5/chirper/app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+def index_view(request):
+    return render(request, 'index.html')
+
+def favorites_view(request):
+    return render(request, 'favorites.html')
