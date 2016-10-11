06d4b27a6989c99107e8d088ee82ddde1fe4d3a8
tdhuynh <tommyhuynh93@gmail.com>
2016-10-11 09:30:02 -0400

    notes from yesterday

--- week5/chirper/app/views.py
+++ week5/chirper/app/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render
 def index_view(request):
     return render(request, 'index.html')
 
+
 def favorites_view(request):
     return render(request, 'favorites.html')
